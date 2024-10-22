a8730eb3d3f8b70d017b167d50fdebee9c029a2c
plethoraBoy <rakeshraja.bee@gmail.com>
2024-10-22 08:49:32 +0530

    ui design

---
+++ src/app/shared/components/card/card.component.ts
@@ -0,0 +1,9 @@
+// card.component.ts
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.scss'],
+})
+export class CardComponent {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { InputComponent } from './components/form-elements/input/input.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MaterialModule } from './components/material.module';
+import { CardComponent } from './components/card/card.component';
 
 @NgModule({
-  declarations: [InputComponent],
+  declarations: [InputComponent,CardComponent],
   imports: [CommonModule, ReactiveFormsModule, MaterialModule],
   exports: [InputComponent, MaterialModule],
 })
